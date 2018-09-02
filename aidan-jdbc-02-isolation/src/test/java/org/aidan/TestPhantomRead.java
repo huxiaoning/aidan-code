fb5a85f3c263bac890700f29581584be647a0e2d
huxiaoningsworld <huxiaoningsworld@sina.com>
2018-09-02 22:19:25 +0800

    aidan-jdbc-01-isolation 模块 测试事务的隔离级别 及解决办法 测试幻读 解决幻读

--- aidan-jdbc-02-isolation/src/test/java/org/aidan/TestPhantomRead.java
+++ aidan-jdbc-02-isolation/src/test/java/org/aidan/TestPhantomRead.java
@@ -9,20 +9,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
- * 测试一下不可重复读:   同一事务两次读取的数据不一致，两次读取中间有其他事务修改并提交了事务
- * 当事务的隔离级别为    读未提交(Read committed)或更低时 会产生不可重复读的问题
- * <p>
- * // * 解决办法：将读写线程的事务隔离级别调成 读已提交或更高级别
- * 操作：在读事务(test1)中的第二次读开始处打断点，先执行读事务 拿到断点后，执行写事务
- * 写事务执行完再放掉读事中的断点 看看两次读的是否一致
+ * 测试一下幻读: 同一事务中两次读取到的数据行数不一致，两次读取期间有其他事务插入或删除了记录，并提交了事务
+ * 产生原因：当事务隔离级别为 可重复读(Repeatable Read)或以下时，会产生幻读的问题
  */
-public class TestUnrepeatableRead {
+public class TestPhantomRead {
     /**
      * 读线程,
      */
     @Test
     public void testRead() {
-        String sql = "select a.name from user a where a.id = ?";
+        String sql = "select count(id) as count from user a";
         Connection connection = DBUtils.getConnection();
         PreparedStatement preparedStatement1 = null;
         PreparedStatement preparedStatement2 = null;
@@ -32,19 +28,18 @@ public class TestUnrepeatableRead {
             // 设置事务不自动提交，手动控制事务
             connection.setAutoCommit(false);
             // 设置 事务的隔离级别为读未提交 将会导致脏读
-//            connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-            // 设置 事务的隔离级别为可重复读 可避免重复读
             connection.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ);
+            // 设置 事务的隔离级别为可重复读 可避免重复读
+//            connection.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ);
             /*
             第一次读
              */
             preparedStatement1 = connection.prepareStatement(sql);
             // 设置sql中的第1个?处的值为1
-            preparedStatement1.setInt(1, 1);
             resultSet1 = preparedStatement1.executeQuery();
             while (resultSet1.next()) {
-                String name = resultSet1.getString("name");
-                System.out.println(name);
+                int count = resultSet1.getInt("count");
+                System.out.println(count);
             }
 
             /*
@@ -52,11 +47,10 @@ public class TestUnrepeatableRead {
              */
             preparedStatement2 = connection.prepareStatement(sql);
             // 设置sql中的第1个?处的值为1
-            preparedStatement2.setInt(1, 1);
             resultSet2 = preparedStatement2.executeQuery();
             while (resultSet2.next()) {
-                String name = resultSet2.getString("name");
-                System.out.println(name);
+                int count = resultSet2.getInt("count");
+                System.out.println(count);
             }
             connection.commit();
         } catch (SQLException e) {
@@ -110,19 +104,18 @@ public class TestUnrepeatableRead {
      */
     @Test
     public void testWrite() {
-        String sql = "update user set name = 'aidan被修改了' where id = ?";
+        String sql = "insert into user (id,name,age) values (100,'jack',25)";
         Connection connection = DBUtils.getConnection();
         PreparedStatement preparedStatement = null;
         try {
             // 设置事务不自动提交，手动控制事务
             connection.setAutoCommit(false);
             // 设置 事务的隔离级别为读未提交 将会导致脏读
-            connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+            connection.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ);
             preparedStatement = connection.prepareStatement(sql);
             // 设置sql中的第1个?处的值为1
-            preparedStatement.setInt(1, 1);
             int i = preparedStatement.executeUpdate();
-            System.out.println("更新了" + i + "条数据");
+            System.out.println("插入了" + i + "条数据");
             connection.commit();
         } catch (SQLException e) {
             try {
